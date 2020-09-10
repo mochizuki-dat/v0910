e65a0e3c3630502047247415bc64fe94cebe39a8
kai-machida <49702001+kai-machida@users.noreply.github.com>
2020-09-10 09:41:47 +0900

    星表示

--- v0910/Form1.cs
+++ v0910/Form1.cs
@@ -12,9 +12,33 @@ namespace v0910
 {
     public partial class Form1 : Form
     {
+
+        int[] vx = new int[3];
+        int[] vy = new int[3];
+        Label[] labels = new Label[3];
+        private static Random rand = new Random();
+
         public Form1()
         {
             InitializeComponent();
+
+            for(int i= 0; i<3;i++)
+            {
+                vx[i] = rand.Next(-15,16);
+                vy[i] = rand.Next(-15,16);
+
+                labels[i] = new Label();
+                labels[i].AutoSize = true;
+                labels[i].Text = "★";
+                labels[i].Left = rand.Next(ClientSize.Width - labels[i].Left);
+                labels[i].Top = rand.Next(ClientSize.Height - labels[i].Top);
+                Controls.Add(labels[i]);
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
         }
     }
 }
